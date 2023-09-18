5e2b2831156d28c3c35f45b5591b572154adcc88
Miguel Nequiz <nekixito@gmail.com>
2023-09-18 11:55:56 -0600

    Se agrega archivo para pruebas

---
+++ probando.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>probando</title>
+	</head>
+	<body>
+		<?php
+
+			$nombre = "Carlos";
+			$edad = 16;
+			$en_edad_escolar = true;
+
+			$edad = $edad + 2;
+
+			echo $nombre . " " . $edad;
+		?>
+	</body>
+</html>
